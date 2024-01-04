e4f6ce264f9f6148222b427f619ee2dfd7781d7c
Changlin Li <mail@changlinli.com>
2024-01-04 14:21:05 -0800

    First iteration of npm installer change
    Change everything to @changlinli for the moment, we'll move to @zokka
    later

--- installers/npm/binary.js
+++ installers/npm/binary.js
@@ -6,19 +6,23 @@ var path = require('path');
 
 // MAIN
 //
-// This function is used by install.js and by the bin/elm backup that gets
+// This function is used by install.js and by the bin/zokka backup that gets
 // called when --ignore-scripts is enabled.
 
 
 module.exports = function()
 {
 	// figure out package of binary
-	var version = package.version.replace(/^(\d+\.\d+\.\d+).*$/, '$1'); // turn '1.2.3-alpha' into '1.2.3'
-	var subPackageName = '@elm_binaries/' + process.platform + '_' + process.arch;
+  // this originally had a regex to calculate a new version by stripping
+  // alpha/beta monikers, but I actually want to keep the subpackages in
+  // lockstep with the main package because ultimately I envision the release
+  // process being automatic
+	var version = package.version;
+  var subPackageName = findSubPackage(process.platform, process.arch);
 
 	verifyPlatform(version, subPackageName);
 
-	var fileName = process.platform === 'win32' ? 'elm.exe' : 'elm';
+	var fileName = process.platform === 'win32' ? 'zokka.exe' : 'zokka';
 
 	try
 	{
@@ -40,17 +44,17 @@ module.exports = function()
 	// so we cannot do any optimizations there
 	var isYarnBerry = /\byarn\/(?!1\.)/.test(process.env.npm_config_user_agent || "");
 
-	// as mentioned in bin/elm we cannot do any optimizations on Windows
+	// as mentioned in bin/zokka we cannot do any optimizations on Windows
 	if (process.platform === 'win32' || isYarnBerry)
 	{
 		return subBinaryPath;
 	}
 
 	// figure out where to put the binary
-	var binaryPath = path.resolve(__dirname, package.bin.elm);
+	var binaryPath = path.resolve(__dirname, package.bin.zokka);
 	var tmpPath = binaryPath + '.tmp';
 
-	// optimize by replacing the JS bin/elm with the native binary directly
+	// optimize by replacing the JS bin/zokka with the native binary directly
 	try
 	{
 		// atomically replace the file with a hard link to the binary
@@ -80,7 +84,7 @@ function verifyPlatform(version, subPackageName)
 		+ 'I am detecting that your computer (' + situation + ') may not be compatible with any\n'
 		+ 'of the official pre-built binaries.\n\n'
 		+ 'I recommend against using the npm installer for your situation. Check out the\n'
-		+ 'alternative installers at https://github.com/elm/compiler/releases/tag/' + version + '\n'
+		+ 'alternative installers at https://github.com/zokka-dev/compiler/releases/tag/' + version + '\n'
 		+ 'to see if there is something that will work better for you.\n\n'
 		+ 'From there I recommend asking for guidance on Slack or Discourse to find someone\n'
 		+ 'who can help with your specific situation.\n\n'
@@ -100,7 +104,7 @@ function exitFailure(version, message)
 		'-- ERROR -----------------------------------------------------------------------\n\n'
 		+ message
 		+ '\n\nNOTE: You can avoid npm entirely by downloading directly from:\n'
-		+ 'https://github.com/elm/compiler/releases/tag/' + version + '\n'
+		+ 'https://github.com/zokka-dev/compiler/releases/tag/' + version + '\n'
 		+ 'All this package does is distribute a file from there.\n\n'
 		+ '--------------------------------------------------------------------------------\n'
 	);
@@ -115,8 +119,25 @@ function exitFailure(version, message)
 function missingSubPackageHelp(subPackageName)
 {
 	return (
-		'I tried to get `elm` from ' + subPackageName + ', but something went wrong.\n'
+		'I tried to get `zokka` from ' + subPackageName + ', but something went wrong.\n'
 		+ 'This can happen if you use the "--omit=optional" or "--no-optional" npm flag, or\n'
 		+ 'if your "node_modules" folder was copied over from a different computer (or VM).\n'
 	);
+}
+
+
+
+// VERIFY
+// This goes away after we fix https://github.com/Zokka-Dev/zokka-compiler/issues/11
+// In particular our need to return the x64 version on darwin instead of arm64
+
+
+function findSubPackage(platform, arch)
+{
+  var packagePrefix = '@changlinli/zokka-binary-';
+  if (platform === 'darwin') {
+    return packagePrefix + 'darwin_x64';
+  } else {
+    return packagePrefix + process.platform + '_' + process.arch;
+  }
 }
