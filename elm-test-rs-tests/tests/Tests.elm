692192ba225538b35195655d4d654d4801c6aa92
Changlin Li <mail@changlinli.com>
2024-07-16 16:49:47 -0700

    Remove BadOccursTest from elm-rs-tests
    This should result in a compilation failure, which is not something that
    test can test for

--- elm-test-rs-tests/tests/Tests.elm
+++ elm-test-rs-tests/tests/Tests.elm
@@ -2,7 +2,6 @@ module Tests exposing (..)
 
 import Test exposing (Test, describe)
 import AnotherBadClosure exposing (anotherBadClosureTest)
-import BadOccursCheck exposing (badOccursCheckTest)
 import TCOProducesBadClosures exposing (tcoProducesBadClosuresTest)
 import TCOMiscompilation0 exposing (tcoMiscompilation0Test)
 -- This causes hanging in vanilla Elm, uncomment once I have a way of failing a test after it runs for too long
@@ -14,14 +13,12 @@ import TCOMiscompilation4 exposing (tcoMiscompilation4Test0, tcoMiscompilation4T
 
 suite : Test
 suite = describe "All tests" 
-    --[ anotherBadClosureTest
-    --, tcoProducesBadClosuresTest
-    --, tcoMiscompilation0Test
-    ---- This causes hanging in vanilla Elm, uncomment once I have a way of failing a test after it runs for too long
-    ---- , tcoMiscompilation1Test
-    --, tcoMiscompilation2Test
-    --, tcoMiscompilation3Test
-    --, tcoMiscompilation4Test0, tcoMiscompilation4Test1
-    --]
-    [ badOccursCheckTest
+    [ anotherBadClosureTest
+    , tcoProducesBadClosuresTest
+    , tcoMiscompilation0Test
+    -- This causes hanging in vanilla Elm, uncomment once I have a way of failing a test after it runs for too long
+    -- , tcoMiscompilation1Test
+    , tcoMiscompilation2Test
+    , tcoMiscompilation3Test
+    , tcoMiscompilation4Test0, tcoMiscompilation4Test1
     ]
